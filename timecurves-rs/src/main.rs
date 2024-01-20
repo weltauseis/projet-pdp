8d4834d2c065f2b77946e3b20d96e34d5ad25d03
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2024-01-20 23:51:13 +0100

    CLI basique

--- timecurves-rs/src/main.rs
+++ timecurves-rs/src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// Specifies the input file for generating the curves.
+    /// The file must be in the correct JSON format, as per the provided template.
+    input: PathBuf,
+
+    /// Specifies the name of the output file where the results will be stored.
+    output: PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    
+    println!("Input file : {}", cli.input.as_path().display());
+    println!("Output file : {}", cli.output.as_path().display());
 }
