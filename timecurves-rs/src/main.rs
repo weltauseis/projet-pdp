dbe2dee19848585de26ea3661f1ac30d32af5571
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2024-01-25 15:27:39 +0100

    formatage + source de l'algo

--- timecurves-rs/src/main.rs
+++ timecurves-rs/src/main.rs
@@ -1,10 +1,9 @@
-use std::{fs::File, ops::Mul, os::unix::process, path::PathBuf};
+use std::{fs::File, ops::Mul, path::PathBuf};
 
 use nalgebra::DMatrix;
 use serde::{Deserialize, Serialize};
 
 use clap::Parser;
-
 
 #[derive(Parser)]
 struct Cli {
@@ -28,29 +27,26 @@ struct InputStruct {
     data: Vec<Data>,
 }
 
-
+// http://www.normalesup.org/~carpenti/Notes/MDS/MDS-simple.html
+// Source : Borg, I., Groenen, P., Modern Multidimensional Scaling : Theory and Applications, Second Edition, Springer, 2005.
 
 //On effectue une opération de double centrage : B = -0.5 * J * P2 *J
 //où J = In - 1/n 1n, In est la matrice unité d'ordre n, 1n est la matrice n x n dont tous les coefficients sont égaux à 1.
 
-
-fn double_centering(matrix : &Vec<Vec<f64>>) ->DMatrix<f64>{
+fn double_centering(matrix: &Vec<Vec<f64>>) -> DMatrix<f64> {
     let n = matrix.len();
     let matrix_i_n = DMatrix::from_diagonal_element(n, n, 1.0);
     let matrix_1_n = DMatrix::from_element(n, n, 1.0);
 
     let mut matrix_squared = DMatrix::from_element(n, n, 0.0);
-    for i in 0..n{
-        for j in 0..n{
-            matrix_squared[(i,j)] = matrix[i][j]*matrix[i][j];
+    for i in 0..n {
+        for j in 0..n {
+            matrix_squared[(i, j)] = matrix[i][j] * matrix[i][j];
         }
     }
-    let matrix_j = matrix_i_n - matrix_1_n.mul(1.0/n as f64);
-    return matrix_j.clone().mul(-0.5) * matrix_squared * matrix_j
+    let matrix_j = matrix_i_n - matrix_1_n.mul(1.0 / n as f64);
+    return matrix_j.clone().mul(-0.5) * matrix_squared * matrix_j;
 }
-
-
-
 
 fn main() {
     let cli = Cli::parse();
@@ -62,9 +58,7 @@ fn main() {
 
     let d: InputStruct = serde_json::from_reader(f).unwrap();
 
-    print!("{}",double_centering(&d.distancematrix));
-
-    
+    print!("{}", double_centering(&d.distancematrix));
 
     println!("Distance Matrix :\n{:?}", d.distancematrix);
 }
