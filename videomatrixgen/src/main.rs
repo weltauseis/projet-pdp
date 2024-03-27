a02e7f37cb5530ec909db30e9217531c07f79e2d
Maxime Granjou <maxime.granjou@etu.u-bordeaux.fr>
2024-03-27 18:04:05 +0100

    fix

--- videomatrixgen/src/main.rs
+++ videomatrixgen/src/main.rs
@@ -6,12 +6,10 @@ use videomatrixgen::create_json_file_from_video;
 async fn main() {
     let args = env::args();
     let args: Vec<String> = args.collect();
-    if args.len() < 4 {
-        println!("Usage: {} <video_path> <image_path> <ouput_path>", args[0]);
+    if args.len() < 2 {
+        println!("Usage: videomatrixgen <video_path>");
         return;
     }
     let video_path = &args[1];
-    let image_path = &args[2];
-    let output_path = &args[3];
-    create_json_file_from_video(video_path, image_path, output_path).await;
+    create_json_file_from_video(video_path, "output", "output.json").await;
 }
