4b3f3a96da9d1a6c95bbab164d1f01d232311bda
TGiloux <thibaultgiloux@hotmail.fr>
2024-04-09 12:22:01 +0200

    Maintenance explained in readme + Export constructor can take size

--- timecurves-py/Readme.md
+++ timecurves-py/Readme.md
@@ -1,16 +1,45 @@
-# Installation
+# Getting started
 
-- python3 -m venv .env
-- source .env/bin/activate
-- pip install maturin
+## Installation
 
-# Build
+- `python3 -m venv .env`
+- `source .env/bin/activate`
+- `pip install maturin`
 
-**source .env/bin/activate** to be in python venv
-Then **maturin develop** to build the project
+## Build
 
-# Use lib in python
+`source .env/bin/activate` to be in python venv
+Then `maturin develop` to build the project
 
-Run python and import the lib as following :
+## Use from cli
+
+Run **src/cli.py [-h] [--format FORMAT] [--algo ALGO] input output**
+
+- FORMAT in { svg, csv, tikz, vegalite }
+- ALGO in { mds }
+
+For exemple :
+`python3 src/cli.py --format vegalite --algo mds ../tcurves/data/template.json output.json`
+
+## Use lib in python
+
+Build the project using maturin then run python and import the lib as following :
 **import timecurves_py**
-or run the file src/test.py to have an overview
+
+# Maintenance
+
+There is a high dependency between timecurve-rs and timecurve-py so any modification from the crate timecurve-rs can break this crate.
+
+### Modification that will break the lib
+
+If any current pub function or any Class that possessed pub function are altered
+
+#### Adding feature
+
+New **Exporter** need to be add in **export** function of _PyExporter_ from _exporter.rs_.
+
+New **ProjectionAlgorithm** need to be add in **new** functon of _PyTimecurve_ from _timecurve.rs_.
+
+Any new **pub method** need to be add and called from the Py*Class*.
+
+Any new **pub class** need to have a new Py*Class* equivalent that embed this new class.
--- timecurves-py/src/export.rs
+++ timecurves-py/src/export.rs
@@ -3,25 +3,30 @@ use pyo3::prelude::*;
 use crate::timecurve::PyTimecurves;
 
 const TIKZ_DRAWING_SIZE: f64 = 10.;
+const VEGA_DRAWING_SIZE: f64 = 400.;
 
 #[pyclass(name = "exporter")]
 pub struct PyExporter {
     pub ext: String,
+    pub size: Option<f64>,
 }
 
 #[pymethods]
 impl PyExporter {
     #[new]
-    fn new(str: Option<&str>) -> Self {
-        let ext = str.unwrap_or("tikz");
+    #[pyo3(signature = (str = "tikz",size = None))]
+    fn new(str: &str, size: Option<f64>) -> Self {
         PyExporter {
-            ext: ext.to_string(),
+            ext: str.to_string(),
+            size,
         }
     }
     fn export(&self, curves: &PyTimecurves) -> PyResult<String> {
         Ok(match self.ext.as_str() {
             "tikz" => timecurves_rs::exporters::Exporter::export(
-                &timecurves_rs::exporters::TikzExporter::new(TIKZ_DRAWING_SIZE),
+                &timecurves_rs::exporters::TikzExporter::new(
+                    self.size.unwrap_or(TIKZ_DRAWING_SIZE),
+                ),
                 &curves.timecurves,
             ),
             "csv" => timecurves_rs::exporters::Exporter::export(
@@ -30,6 +35,12 @@ impl PyExporter {
             ),
             "svg" => timecurves_rs::exporters::Exporter::export(
                 &timecurves_rs::exporters::SVGExporter::new(),
+                &curves.timecurves,
+            ),
+            "vegalite" => timecurves_rs::exporters::Exporter::export(
+                &timecurves_rs::exporters::VegaLiteExporter::new(
+                    self.size.unwrap_or(VEGA_DRAWING_SIZE) as u64,
+                ),
                 &curves.timecurves,
             ),
             _ => {
