260a671d1e1d33b65824fcc4d58191a55ce1e12b
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2024-04-20 15:40:18 +0200

    updated to latest pyo3

--- timecurves-py/src/lib.rs
+++ timecurves-py/src/lib.rs
@@ -4,7 +4,7 @@ mod input;
 mod timecurve;
 
 #[pymodule]
-fn timecurves_py(_py: Python, m: &PyModule) -> PyResult<()> {
+fn timecurves_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<export::PyExporter>()?;
     //Input
     m.add_function(wrap_pyfunction!(input::input_from_filename, m)?)?;
