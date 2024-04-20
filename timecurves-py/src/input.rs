a2eea64f927795059854809e7b3402103b3f3288
Kevin Jourdain <kevin.jourdain@etu.u-bordeaux.fr>
2024-04-20 16:22:48 +0200

    weird binding for input

--- timecurves-py/src/input.rs
+++ timecurves-py/src/input.rs
@@ -1,41 +1,68 @@
 use pyo3::{exceptions::PyValueError, prelude::*};
 use timecurves_rs::input::{Dataset, InputData};
 
-#[pyclass(name = "inputdata")]
-pub struct PyInputData {
-    pub inputdata: InputData,
+#[pyclass(name = "Dataset")]
+#[derive(Clone)]
+pub struct PyDataset {
+    inner: Dataset,
 }
+
+impl From<Dataset> for PyDataset {
+    fn from(d: Dataset) -> Self {
+        PyDataset { inner: d }
+    }
+}
+
 #[pymethods]
-impl PyInputData {}
-
-#[pyfunction]
-pub fn input_from_dataset(
-    dmatrix: Vec<Vec<f64>>,
-    datasets: Vec<(String, Vec<String>)>,
-) -> PyResult<PyInputData> {
-    let datasets = datasets
-        .iter()
-        .map(|(name, timelabels)| Dataset {
-            name: name.clone(),
-            timelabels: timelabels.clone(),
-        })
-        .collect();
-
-    Ok(PyInputData {
-        inputdata: InputData::from(dmatrix, datasets),
-    })
+impl PyDataset {
+    #[new]
+    pub fn new(name: &str, timelabels: Vec<String>) -> Self {
+        PyDataset {
+            inner: Dataset {
+                name: name.to_string(),
+                timelabels,
+            },
+        }
+    }
 }
+
+#[pyclass(name = "InputData")]
+pub struct PyInputData {
+    inner: InputData,
+}
+
+impl From<InputData> for PyInputData {
+    fn from(d: InputData) -> Self {
+        PyInputData { inner: d }
+    }
+}
+
+#[pymethods]
+impl PyInputData {
+    #[new]
+    pub fn new(distancematrix: Vec<Vec<f64>>, data: Vec<PyDataset>) -> Self {
+        PyInputData {
+            inner: InputData {
+                distancematrix,
+                data: data.into_iter().map(|d| d.inner).collect(),
+            },
+        }
+    }
+}
+
+// functions to allow for "multiple constructors"
 #[pyfunction]
 pub fn input_from_filename(filename: &str) -> PyResult<PyInputData> {
     match InputData::from_filename(filename) {
-        Ok(v) => Ok(PyInputData { inputdata: v }),
+        Ok(v) => Ok(v.into()),
         Err(e) => Err(PyValueError::new_err(e.to_string())),
     }
 }
+
 #[pyfunction]
 pub fn input_from_str(string: &str) -> PyResult<PyInputData> {
     match InputData::from_str(string) {
-        Ok(v) => Ok(PyInputData { inputdata: v }),
+        Ok(v) => Ok(v.into()),
         Err(e) => Err(PyValueError::new_err(e.to_string())),
     }
 }
--- timecurves-py/src/lib.rs
+++ timecurves-py/src/lib.rs
@@ -1,15 +1,16 @@
 use pyo3::prelude::*;
 //mod export;
-//mod input;
+mod input;
 mod timecurve;
 
 #[pymodule]
 fn timecurves_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    /* m.add_class::<export::PyExporter>()?;
     //Input
+    m.add_class::<input::PyDataset>()?;
+    m.add_class::<input::PyInputData>()?;
     m.add_function(wrap_pyfunction!(input::input_from_filename, m)?)?;
     m.add_function(wrap_pyfunction!(input::input_from_str, m)?)?;
-    m.add_function(wrap_pyfunction!(input::input_from_dataset, m)?)?;
+    /* m.add_class::<export::PyExporter>()?;
     //Timecurve
     m.add_function(wrap_pyfunction!(timecurve::timecurves_from_data, m)?)?; */
 
